7cd18c395d0d79e137b2eb97d5b59bbb703c1020
Elena Torro <elenatorro@gmail.com>
2014-08-27 11:30:10 +0200

    controller created for printing the document

---
+++ app/controllers/document_controller.rb
@@ -0,0 +1,51 @@
+class DocumentController < ApplicationController
+
+  def index
+  	if current_project
+      @site_requirements = get_site_requirements
+      @personas = get_personas
+      @card_sorting_images = get_card_sorting_images
+      @conduct_task_analysis = get_conduct_task_analysis
+      @use_cases = get_use_cases
+      @user_requirements = get_user_requirements
+  		render "index", :layout => false
+  	else 
+  		redirect_to root_path
+  	end
+  end
+
+  private
+  def get_site_requirements
+    requirements = [current_project.site_requirement.requirement1,
+                    current_project.site_requirement.requirement2,
+                    current_project.site_requirement.requirement3,
+                    current_project.site_requirement.requirement4,
+                    current_project.site_requirement.requirement5,
+                    current_project.site_requirement.requirement6,
+                    current_project.site_requirement.requirement7,
+                    current_project.site_requirement.requirement8,
+                    current_project.site_requirement.requirement9,
+                    current_project.site_requirement.requirement10]
+    
+  end
+
+  def get_personas
+    current_project.personas
+  end
+
+  def get_card_sorting_images
+    current_project.card_images
+  end
+
+  def get_conduct_task_analysis
+    current_project.analysis
+  end
+
+  def get_use_cases
+    current_project.use_cases
+  end
+
+  def get_user_requirements
+    current_project.user_requirements
+  end
+end
